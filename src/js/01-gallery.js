cbaf9fe6dca5a5fd9634704860d48e1527916018
Kseniia111 <kseniya.markovych@gmail.com>
2023-07-28 14:54:35 +0300

    First Task and Libraries

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const galleryMarkup = document.querrySelector('.gallery');
+
+const galleryEl = galleryItems
+    .map(({ preview, original, description }) =>
+        `<li class = "gallery__item">
+    <a class = "gallery__link" href = "${original}">
+    <img class = "gallery__image"
+    src = "${preview}"
+    data-source = "${original}"
+    alt = "${description}"
+    />
+    </a>
+    </li>`)
+    .join("");
+
+    gallery.addEventListener('click', onImgClick)
+
+function onImgClick(evt) {
+  evt.preventDefault();
+  
+  const itemLink = evt.target;
+  if (evt.target.nodeName !== 'IMG') {
+        return;
+  }
+  
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+
+  console.dir(instance);
+
+  galleryEl.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
